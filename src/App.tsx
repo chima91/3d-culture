e39b55a28b8a6e55bad68cf47a8448b125149e99
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-06 21:08:34 +0900

    test of react-three-fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 // import React from 'react';
+import { useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
 
-function App() {
+const Thing = () => {
+  const ref: any = useRef();
+  useFrame(() => ref.current.rotation.z += 0.05);
   return (
-    <h1>test</h1>
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}
+    >
+      <planeBufferGeometry attach='geometry' args={[1, 1]} />
+      <meshBasicMaterial
+        attach='material'
+        color='hotpink'
+        opacity={0.5}
+        transparent
+      />
+    </mesh>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <h1>react-three-fiberのテスト</h1>
+      <Canvas>
+        <Thing />
+      </Canvas>
+    </>
   );
 }
 
