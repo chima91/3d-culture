f553ee6b4ee58497f84067545dfb746c368896de
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-08 19:12:40 +0900

    install @material-ui/core && setup of material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+
 import App from './App';
+
+const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
