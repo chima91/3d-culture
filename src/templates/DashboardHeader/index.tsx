7f8506ddf3b86a9ae6ad034a602ec5a1f7e86c8e
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-09 21:51:07 +0900

    create SearchBar

--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -2,6 +2,7 @@ import { AppBar, IconButton, Toolbar } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 
 import { Logo } from "../../components/Logo";
+import { SearchBar } from "./SearchBar";
 import useStyles from "./style";
 
 export const DashboardHeader = () => {
@@ -17,6 +18,9 @@ export const DashboardHeader = () => {
           <div className={styles.logo}>
             <Logo />
           </div>
+
+          <SearchBar />
+
         </Toolbar>
       </AppBar>
     </div>
