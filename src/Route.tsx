f53cf266ccb684f2d3333a5776b8f6d780383fef
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-09 09:46:55 +0900

    create Route with useRoutes

---
+++ src/Route.tsx
@@ -0,0 +1,32 @@
+import { useRoutes, Navigate } from "react-router-dom";
+
+import { HomeLayout } from "./layouts/Home";
+import { SideLessHomeLayout } from "./layouts/SidelessHome";
+import { SimpleLayout } from "./layouts/Simple";
+
+export const RootRouter = () => {
+  return useRoutes([
+    {
+      element: <HomeLayout />,
+      children: [{ path: '/', element: <div>Home</div> }],
+    },
+    {
+      element: <SideLessHomeLayout />,
+      children: [
+        { path: "detail", element: <Navigate to='/' /> },
+        { path: "detail/:objId", element: <div>detail</div> }
+      ],
+    },
+    {
+      element: <SimpleLayout />,
+      children: [
+        { path: 'login', element: <div>ログイン</div> },
+        { path: 'signup', element: <div>アカウント新規作成</div> },
+        { path: 'forget', element: <div>パスワードリセット</div> },
+        { path: '404', element: <div>Not Found</div> },
+        { path: '*', element: <Navigate to='/404' /> }
+      ]
+    },
+    { path: '*', element: <Navigate to='/404' /> }
+  ])
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { RootRouter } from './Route';
 
 const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <RootRouter />
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
