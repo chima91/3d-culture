15344904b4bc9873501f61fb85bde3ad8a5b3db1
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-09 10:09:53 +0900

    add Logo style

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,3 +1,13 @@
+import useStyles from "./style";
+
 export const Logo = () => {
-  return <img src="/static/logo.png" alt="Culpticon Logo" />;
+  const styles = useStyles();
+
+  return (
+    <img
+      className={styles.root}
+      src="/static/logo.png"
+      alt="Culpticon Logo"
+    />
+  )
 }
