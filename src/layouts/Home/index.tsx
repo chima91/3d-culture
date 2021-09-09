da118a39edfa16bd1ba0515651e1d1bcf656fcb8
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-09 09:55:40 +0900

    create Home layout

---
+++ src/layouts/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+import { DashboardHeader } from "../../templates/DashboardHeader";
+
+export const HomeLayout = () => {
+  return (
+    <div>
+      <DashboardHeader />
+      <Outlet />
+    </div>
+  );
+};
