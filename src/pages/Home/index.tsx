4ba94af5d802991e3fd392394b03a2f86659bf90
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-13 21:35:24 +0900

    add Link to detail page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,33 +1,37 @@
 import { Container, Grid } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 import { ObjCard } from "../../components/ObjCard";
+import useStyles from "./style";
 
 export const Home = () => {
+  const styles = useStyles();
+
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
-        <Grid item xs={12} md={6} lg={4}>
+        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
           <ObjCard />
         </Grid>
       </Grid>
