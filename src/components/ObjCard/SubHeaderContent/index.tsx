67a8035ec06b5fe5a80033e68310be2659864cd3
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-13 11:02:44 +0900

    create SubHeaderContent of card component

---
+++ src/components/ObjCard/SubHeaderContent/index.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@material-ui/core";
+
+export const SubHeaderContent = () => {
+  return (
+    <>
+      <Typography variant="body2">あいうえ博物館</Typography>
+      <Typography variant="body2">2021/06/28</Typography>
+    </>
+  );
+};
