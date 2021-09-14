a6cead61a2d5b1bb56c35840ec0a4c206185c790
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-14 22:56:52 +0900

    amend demo 3D

--- src/components/Three/index.tsx
+++ src/components/Three/index.tsx
@@ -3,22 +3,14 @@ import { useFrame } from "react-three-fiber";
 
 export const Three = () => {
   const ref: any = useRef();
-  useFrame(() => ref.current.rotation.z += 0.05);
+  useFrame(() => {
+    ref.current.rotation.y += 0.01;
+  });
 
   return (
-    <mesh
-      ref={ref}
-      onClick={e => console.log('click')}
-      onPointerOver={e => console.log('hover')}
-      onPointerOut={e => console.log('unhover')}
-    >
-      <planeBufferGeometry attach='geometry' args={[1, 1]} />
-      <meshBasicMaterial
-        attach='material'
-        color='hotpink'
-        opacity={0.5}
-        transparent
-      />
+    <mesh ref={ref}>
+      <boxGeometry attach="geometry" args={[400, 400, 400]} />
+      <meshNormalMaterial attach='material' />
     </mesh>
   )
 };
