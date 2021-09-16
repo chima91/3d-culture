06ac76c92668795ac4a281e309469d9006458661
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-16 23:32:47 +0900

    import @types/three, camera-controls

--- src/components/Three/index.tsx
+++ src/components/Three/index.tsx
@@ -1,5 +1,7 @@
 import { useRef } from "react";
 import { useFrame } from "react-three-fiber";
+import * as THREE from "three";
+import CameraControls from "camera-controls";
 
 export const Three = () => {
   const ref: any = useRef();
