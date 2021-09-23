b274c272045a05d7add41790f09dbbde6836ac38
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-23 23:32:27 +0900

    add props to ObjCard component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,29 +10,29 @@ export const Home = () => {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
+        <Grid item className={styles.underline} component={Link} to="/detail/1" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/haniwa.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
+        </Grid>
+        <Grid item className={styles.underline} component={Link} to="/detail/2" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/no-image.jpg" title="踊らないハニワ" owner="かきくけ博物館" created={new Date()} />
+        </Grid>
+        <Grid item className={styles.underline} component={Link} to="/detail/3" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/haniwa.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
+        </Grid>
+        <Grid item className={styles.underline} component={Link} to="/detail/4" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/no-image.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
         </Grid>
         <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
+          <ObjCard imageUrl="/static/haniwa.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
         </Grid>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
+        <Grid item className={styles.underline} component={Link} to="/detail/6" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/haniwa.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
         </Grid>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
+        <Grid item className={styles.underline} component={Link} to="/detail/7" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/no-image.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
         </Grid>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
-        </Grid>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
-        </Grid>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
-        </Grid>
-        <Grid item className={styles.underline} component={Link} to="/detail/5" xs={12} md={6} lg={4}>
-          <ObjCard />
+        <Grid item className={styles.underline} component={Link} to="/detail/8" xs={12} md={6} lg={4}>
+          <ObjCard imageUrl="/static/haniwa.jpg" title="踊るハニワ" owner="あいうえ博物館" created={new Date()} />
         </Grid>
       </Grid>
     </Container>
