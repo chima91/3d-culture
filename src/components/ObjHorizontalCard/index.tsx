b388c217ff7203fc0072dc56b2424d2e19512a84
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-24 11:06:08 +0900

    add props

--- src/components/ObjHorizontalCard/index.tsx
+++ src/components/ObjHorizontalCard/index.tsx
@@ -1,19 +1,22 @@
 import { Card, CardHeader, CardMedia } from "@material-ui/core";
 
-import { HeaderTitle } from "../ObjCard/HeaderTitle";
-import { SubHeaderContent } from "../ObjCard/SubHeaderContent";
-
+import { HeaderTitle, HeaderTitleProps } from "../ObjCard/HeaderTitle";
+import { SubHeaderContent, SubHeaderContentProps } from "../ObjCard/SubHeaderContent";
 import useStyles from "./style";
 
-export const ObjHorizontalCard = () => {
+export type ObjHorizontalCardProps = {
+  imageUrl: string;
+} & HeaderTitleProps & SubHeaderContentProps;
+
+export const ObjHorizontalCard = ({ imageUrl, title, owner, created }: ObjHorizontalCardProps) => {
   const styles = useStyles();
 
   return (
     <Card className={`${styles.root} ${styles.transparent}`} elevation={0} square>
       <div className={styles.thumbnail}>
-        <CardMedia className={styles.media} image="/static/no-image.jpg" title="Thumbnail" />
+        <CardMedia className={styles.media} image={imageUrl} title="Thumbnail" />
       </div>
-      <CardHeader className={styles.contentPadding} title={<HeaderTitle />} subheader={<SubHeaderContent />} />
+      <CardHeader className={styles.contentPadding} title={<HeaderTitle title={title} />} subheader={<SubHeaderContent owner={owner} created={created} />} />
     </Card>
   );
 };
