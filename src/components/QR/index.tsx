7e31bc489f5949edd498c5623ff023b30df65d20
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-25 11:42:12 +0900

    install react-qrcodes & create QR component

---
+++ src/components/QR/index.tsx
@@ -0,0 +1,15 @@
+import { useQRCode } from "react-qrcodes";
+
+export const QR = () => {
+  const [inputRef]: any = useQRCode({
+    text: window.location.href,
+    options: {
+      level: 'M', // 誤り訂正レベル
+      margin: 3, // QRコードの周りの空白マージン
+      scale: 1,
+      width: 200,
+    }
+  });
+
+  return <canvas ref={inputRef} />;
+}
