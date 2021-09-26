4a38c659502f0bca03d21ec4f5c060bfe55b9c00
Kazuma NAGAI <itb4769we@gmail.com>
2021-09-26 10:09:40 +0900

    add style to fallback text

--- src/components/Three/index.tsx
+++ src/components/Three/index.tsx
@@ -6,7 +6,7 @@ import { Model } from "../Model";
 
 export const Three = () => {
   return (
-    <React.Suspense fallback={<span>loading...</span>}>
+    <React.Suspense fallback={<div style={{ color: "white", textAlign: "center", marginTop: 100 }}>Now Loading...</div>}>
       <Fiber.Canvas>
         <Drei.PerspectiveCamera makeDefault fov={15} />
         <Drei.OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
