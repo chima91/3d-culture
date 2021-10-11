ef46f3d2d01807fa0fd7131cbf3372f2ce3a7b3b
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-11 16:06:12 +0900

    create forgetPassForm page

---
+++ src/pages/forgetPassForm/index.tsx
@@ -0,0 +1,43 @@
+import { Button, Card, TextField, Typography } from "@material-ui/core";
+
+import { Logo } from "../../components/Logo";
+import useStyles from "./style";
+
+export const ForgetPassForm = () => {
+  const styles = useStyles();
+
+  return (
+    <Card className={styles.root} variant="outlined">
+      <div className={`${styles.logo} ${styles.margin}`}>
+        <Logo />
+      </div>
+
+      <Typography className={styles.margin} component="h1" variant="h5">
+        パスワードの再発行
+      </Typography>
+
+      <label className={`${styles.label} ${styles.margin}`}>
+        <Typography>メールアドレス</Typography>
+        <TextField
+          type="email"
+          required
+          size="small"
+          fullWidth
+          variant="outlined"
+        />
+      </label>
+
+      <div className={styles.margin}>
+        <Button variant="contained" color="primary">
+          再発行
+        </Button>
+      </div>
+
+      <div>
+        <Button href="/login" color="primary">
+          ログインはこちら
+        </Button>
+      </div>
+    </Card>
+  );
+};
