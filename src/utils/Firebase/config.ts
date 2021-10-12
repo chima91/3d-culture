2b176e67251b5401998871d75ee5bf00b6472491
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-12 17:51:43 +0900

    setup of firebase

---
+++ src/utils/Firebase/config.ts
@@ -0,0 +1,20 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcaCwPXi3F4a0uz3JGkMZNIy3i9PmVNS8",
+  authDomain: "react-3d-f49c6.firebaseapp.com",
+  projectId: "react-3d-f49c6",
+  storageBucket: "react-3d-f49c6.appspot.com",
+  messagingSenderId: "419875646774",
+  appId: "1:419875646774:web:ab2657b7d6289c74d59797",
+  measurementId: "G-GCMW5GDSDY"
+};
+
+// apiKeyでfirebaseパッケージを初期化
+firebase.initializeApp(firebaseConfig);
+
+export const fireAuth = firebase.auth();
+export const storage = firebase.storage();
+
+// 初期化済みのfirebaseパッケージをexport
+export default firebase;
