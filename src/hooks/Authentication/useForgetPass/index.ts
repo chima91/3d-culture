fe63662aaec0bdd6e5c07195b67844f6ef62f3f2
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-19 12:30:18 +0900

    create useForgetPass hooks using useAuthHelper hooks

---
+++ src/hooks/Authentication/useForgetPass/index.ts
@@ -0,0 +1,40 @@
+import { useRef, useState } from "react";
+import { SetErrorFn, useAuthHelper } from "../useAuthHelper";
+import { forgetPass } from "../../../utils/Firebase/forgetPass";
+
+export const useForgetPass = () => {
+  // ユーザーが入力した値を読み取るための`ref`
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const [sendSuccess, setSendSuccess] = useState<boolean>(false);
+
+  const formValidation = (setError: SetErrorFn) => {
+    if (!emailRef.current?.value) {
+      setError("email", "メールアドレスを入力してください。");
+      return true;
+    }
+
+    return false;
+  };
+
+  const sendEmail = async() => {
+    await forgetPass(emailRef.current?.value || "");
+    setSendSuccess(true);
+  };
+
+  // useAuthHelperを使用して、実際に認証に使用する関数を生成する
+  const { authExecute, error, loading } = useAuthHelper(
+    sendEmail,
+    formValidation
+  );
+
+  return {
+    ref: {
+      emailRef,
+    },
+    loading,
+    error,
+    sendEmail: authExecute,
+    sendSuccess,
+  }
+}
