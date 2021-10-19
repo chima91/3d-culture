84143e5b8e3207b7263ce9067541ac26c29ecaf9
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-19 12:11:14 +0900

    add Signout component

--- src/Route.tsx
+++ src/Route.tsx
@@ -7,6 +7,7 @@ import { Detail } from "./pages/Detail";
 import { ForgetPassForm } from "./pages/ForgetPassForm";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
+import { Signout } from "./pages/Signout";
 import { Signup } from "./pages/Signup";
 
 export const RootRouter = () => {
@@ -27,6 +28,7 @@ export const RootRouter = () => {
       children: [
         { path: 'login', element: <Login /> },
         { path: 'signup', element: <Signup /> },
+        { path: 'signout', element: <Signout /> },
         { path: 'forget', element: <ForgetPassForm /> },
         { path: '404', element: <div>Not Found</div> },
         { path: '*', element: <Navigate to='/404' /> }
