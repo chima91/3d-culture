c1a5e3603bf8b6282b1d8ba947033c31d684a364
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-20 18:47:24 +0900

    add Token to headers of ApolloClient

--- src/providers/ApolloClient/index.tsx
+++ src/providers/ApolloClient/index.tsx
@@ -2,17 +2,25 @@ import { ApolloProvider as Provider, ApolloClient, createHttpLink, InMemoryCache
 import { setContext } from "@apollo/client/link/context";
 import { PropsWithChildren } from "react";
 
+import { fireAuth } from "../../utils/Firebase/config";
+
 // GraphQL APIのエンドポイントを指定する
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_GRAPHQL_END_POINT_ORIGIN,
 });
 
 const authLink = setContext(async () => {
-  return {
-    headers: {
-      "x-hasura-admin-secret": process.env.REACT_APP_HASURA_SECRET_KEY,
-    }
-  }
+  const token = await fireAuth.currentUser?.getIdToken(true);
+
+  // Bearerトークンでトークンを送信する
+  // headersのプロパティは`Authorization`
+  const headers = token ? { Authorization: `Bearer ${token}` } : {};
+  return { headers };
+  // return {
+  //   headers: {
+  //     "x-hasura-admin-secret": process.env.REACT_APP_HASURA_SECRET_KEY,
+  //   }
+  // }
 });
 
 const apolloClient = new ApolloClient({
