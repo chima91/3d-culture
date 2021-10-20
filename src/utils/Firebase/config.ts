22e22d02154c6b4baef5b82e771f9c46cef009a8
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-20 10:05:04 +0900

    fix how to import firebase

--- src/utils/Firebase/config.ts
+++ src/utils/Firebase/config.ts
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBcaCwPXi3F4a0uz3JGkMZNIy3i9PmVNS8",
