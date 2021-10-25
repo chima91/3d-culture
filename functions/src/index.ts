08464c623559c626730372b0d508cc02d95348d0
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-25 11:10:55 +0900

    delete comments

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,9 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-// firebaseの初期化
 admin.initializeApp(functions.config().firebase);
 
-/**
- * Firebase FunctionsにprocessSignUpと言う名前の関数を作成
- * `functions.auth.user().onCreate`で、Authenticationでuserが作成された時に実行される関数を定義
- */
 exports.processSignUp = functions.auth.user().onCreate((user) => {
-  // Hasura用のカスタムクレームの作成
   const customClaims = {
     "https://hasura.io/jwt/claims": {
       "x-hasura-default-role": "user",
@@ -18,7 +12,6 @@ exports.processSignUp = functions.auth.user().onCreate((user) => {
     },
   };
 
-  // userのトークンにカスタムクレームを追加する
   return admin
     .auth()
     .setCustomUserClaims(user.uid, customClaims)
@@ -29,4 +22,4 @@ exports.processSignUp = functions.auth.user().onCreate((user) => {
         .doc(user.uid)
         .set({ refreshTime: admin.firestore.FieldValue.serverTimestamp() });
     });
-});
+})
