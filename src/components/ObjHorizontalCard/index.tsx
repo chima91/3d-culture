ae2a77358fcdb8194b6dd6f169b1d43efdf5a04c
Kazuma NAGAI <itb4769we@gmail.com>
2021-10-25 17:46:52 +0900

    add props

--- src/components/ObjHorizontalCard/index.tsx
+++ src/components/ObjHorizontalCard/index.tsx
@@ -1,22 +1,33 @@
 import { Card, CardHeader, CardMedia } from "@material-ui/core";
+import { useState, useEffect } from "react";
 
 import { HeaderTitle, HeaderTitleProps } from "../ObjCard/HeaderTitle";
 import { SubHeaderContent, SubHeaderContentProps } from "../ObjCard/SubHeaderContent";
 import useStyles from "./style";
 
 export type ObjHorizontalCardProps = {
-  imageUrl: string;
+  fetcher: () => Promise<string | undefined>;
 } & HeaderTitleProps & SubHeaderContentProps;
 
-export const ObjHorizontalCard = ({ imageUrl, title, owner, created }: ObjHorizontalCardProps) => {
+export const ObjHorizontalCard = ({ fetcher, title, owner, created }: ObjHorizontalCardProps) => {
   const styles = useStyles();
+  // サムネイルのダウンロードリンクのステート
+  const [src, setSrc] = useState<string>();
+  useEffect(() => {
+    // サムネイルのダウンロードリンクを取得する関数
+    fetcher().then(setSrc);
+  });
 
   return (
     <Card className={`${styles.root} ${styles.transparent}`} elevation={0} square>
       <div className={styles.thumbnail}>
-        <CardMedia className={styles.media} image={imageUrl} title="Thumbnail" />
+        <CardMedia className={styles.media} image={src} title="Thumbnail" />
       </div>
-      <CardHeader className={styles.contentPadding} title={<HeaderTitle title={title} />} subheader={<SubHeaderContent owner={owner} created={created} />} />
+      <CardHeader
+        className={styles.contentPadding}
+        title={<HeaderTitle title={title} />}
+        subheader={<SubHeaderContent owner={owner} created={created} />}
+      />
     </Card>
   );
 };
