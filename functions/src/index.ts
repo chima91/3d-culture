975d3c7c1fec2cbd4e64f6643a70ca3e9bfa9fd1
web-novice-tech <itb4769we@gmail.com>
2021-11-25 22:04:10 +0900

    add async func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,22 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { firestore } from "firebase-admin";
 
 admin.initializeApp(functions.config().firebase);
 
-exports.processSignUp = functions.auth.user().onCreate((user) => {
-  const customClaims = {
-    "https://hasura.io/jwt/claims": {
-      "x-hasura-default-role": "user",
-      "x-hasura-allowed-roles": ["user"],
-      "x-hasura-user-id": user.uid,
-    },
-  };
-
-  return admin
-    .auth()
-    .setCustomUserClaims(user.uid, customClaims)
-    .then(() => {
-      return admin
-        .firestore()
-        .collection("users")
-        .doc(user.uid)
-        .set({ refreshTime: admin.firestore.FieldValue.serverTimestamp() });
-    });
-})
+exports.processSignUp = functions.auth.user().onCreate(async user => {
+  return admin.auth().setCustomUserClaims(user.uid, {
+    'https://hasura.io/jwt/claims': {
+    'x-hasura-default-role': 'user',
+    'x-hasura-allowed-roles': ['user'],
+    'x-hasura-user-id': user.uid
+  }})
+  .then(async () => {
+    await firestore().collection('users').doc(user.uid).set({
+      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    })
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
