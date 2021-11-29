5c01a290fd2f04df8df284231ff686d10408c5ea
web-novice-tech <itb4769we@gmail.com>
2021-11-29 14:41:50 +0900

    #15 create useSubscribe/useUnSubscribe hooks

---
+++ src/hooks/Channel/useUnSubscribe/index.ts
@@ -0,0 +1,39 @@
+import {
+  useDeleteSubscribeMutation,
+  SubscribersDocument,
+  UserByIdDocument
+} from "../../../utils/graphql/generated";
+
+type UnSubscribeProps = {
+  userid: string;
+  subscribeId: string;
+};
+
+export const useUnSubscribe = () => {
+  const [deleteSubscription, { data, error }] = useDeleteSubscribeMutation({
+    refetchQueries: [UserByIdDocument, SubscribersDocument],
+  });
+
+  const unsubscribe = async ({ userid, subscribeId }: UnSubscribeProps) => {
+    if (userid && subscribeId) {
+      try {
+        await deleteSubscription({
+          variables: {
+            userid: userid,
+            subscribe_id: subscribeId
+          }
+        });
+
+        if (error) console.log(error.message);
+      } catch (e) {
+        new Error("チャンネル登録の解除に失敗しました。");
+      }
+    }
+  };
+
+  return {
+    unsubscribe,
+    data,
+    error,
+  };
+};
