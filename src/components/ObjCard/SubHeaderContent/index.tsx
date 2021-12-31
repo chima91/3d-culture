054d629481cb637f3f0b242897d350fe8b0d6778
web-novice-tech <itb4769we@gmail.com>
2021-12-31 17:50:20 +0900

    create Channels page

--- src/components/ObjCard/SubHeaderContent/index.tsx
+++ src/components/ObjCard/SubHeaderContent/index.tsx
@@ -1,16 +1,16 @@
 import { Typography } from "@material-ui/core";
 
 export type SubHeaderContentProps = {
-  owner: string;
-  created: Date;
-  views: number;
+  owner?: string;
+  created?: Date;
+  views?: number;
 }
 
 export const SubHeaderContent = ({owner, created, views}: SubHeaderContentProps) => {
   return (
     <>
       <Typography variant="body2">{owner}</Typography>
-      <Typography variant="body2">{new Date(created).toLocaleDateString()}</Typography>
+      <Typography variant="body2">{created && new Date(created).toLocaleDateString()}</Typography>
       <Typography variant="body2">閲覧回数：{views}回</Typography>
     </>
   );
