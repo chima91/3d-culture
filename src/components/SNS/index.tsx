683afd47842ccc41e709808a5c4de660df4e77b0
web-novice-tech <itb4769we@gmail.com>
2022-02-25 10:23:27 +0900

    'YouTube'という文言を削除

--- src/components/SNS/index.tsx
+++ src/components/SNS/index.tsx
@@ -10,13 +10,13 @@ import {
 export const SNS = () => {
   return (
     <span style={{ marginLeft: 15 }}>
-      <TwitterShareButton url={window.location.href} title='文化財のYouTube「Culpticon」で3Dの文化財を楽しもう!!!!' style={{ margin: 5 }}>
+      <TwitterShareButton url={window.location.href} title='「Culpticon」で3Dの文化財を楽しもう!' style={{ margin: 5 }}>
         <TwitterIcon size={32} round />
       </TwitterShareButton>
-      <LineShareButton url={window.location.href} title='文化財のYouTube「Culpticon」で3Dの文化財を楽しもう!!!!' style={{ margin: 5 }}>
+      <LineShareButton url={window.location.href} title='「Culpticon」で3Dの文化財を楽しもう!' style={{ margin: 5 }}>
         <LineIcon size={32} round />
       </LineShareButton>
-      <FacebookShareButton url={window.location.href} quote='文化財のYouTube「Culpticon」で3Dの文化財を楽しもう!!!!' style={{ margin: 5 }}>
+      <FacebookShareButton url={window.location.href} quote='「Culpticon」で3Dの文化財を楽しもう!' style={{ margin: 5 }}>
         <FacebookIcon size={32} round />
       </FacebookShareButton>
     </span>
