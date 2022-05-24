df9d838066feb188aeb8c0da1d858524a76b5acf
web-novice-tech <itb4769we@gmail.com>
2022-05-24 09:23:03 +0900

    style(/): requirePragmaをfalseに設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import { RootRouter } from "./Route";
-import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
-import GlobalStyle from "./GlobalStyle";
-import { RecoilRoot } from "recoil";
-import { AuthStateListener } from "./providers/AuthStateListener";
-import { GlobalAccount } from "./providers/GlobalAccount";
-import { ApolloProvider } from "./providers/ApolloClient";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { RootRouter } from './Route';
+import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+import GlobalStyle from './GlobalStyle';
+import { RecoilRoot } from 'recoil';
+import { AuthStateListener } from './providers/AuthStateListener';
+import { GlobalAccount } from './providers/GlobalAccount';
+import { ApolloProvider } from './providers/ApolloClient';
 
-// Material-UIの「テーマ」を作成する。
-// Material-UIをカスタマイズする際には、createThemeの引数にカスタマイズ項目を渡す。
+// Material-UIの「テーマ」を作成。
 const theme = createTheme();
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*
-      Recoilを初期化し、アプリケーション全体でRecoilを使用できるようにする
-    */}
+    {/* Recoilを初期化し、アプリケーション全体でRecoilを使用できるようにする */}
     <RecoilRoot>
-      {/*
-      Material-UIを初期化し、アプリケーション全体でMaterial-UIを使用できるようにする
-    */}
+      {/* Material-UIを初期化し、アプリケーション全体でMaterial-UIを使用できるようにする */}
       <ThemeProvider theme={theme}>
-        {/*
-        Apollo Clientを初期化して、アプリケーション全体でApollo Clientを使えるようにする
-      */}
+        {/* Apollo Clientを初期化して、アプリケーション全体でApollo Clientを使えるようにする */}
         <ApolloProvider>
-          {/*
-            ユーザーの認証情報を読む込み
-          */}
+          {/* ユーザーの認証情報を読む込み */}
           <AuthStateListener>
-            {/*
-            ユーザー情報を読む込み
-          */}
+            {/* ユーザー情報を読む込み */}
             <GlobalAccount>
               <BrowserRouter>
-                {/*
-              ブラウザの違いを吸収し、どのデバイスでも同じように表示する用のCSSを使用する
-              */}
+                {/* ブラウザの違いを吸収し、どのデバイスでも同じように表示する用のCSSを使用する */}
                 <CssBaseline />
-
-                {/*
-                アプリ全体の特殊なグローバルスタリング
-              */}
+                {/* アプリ全体の特殊なグローバルスタリング */}
                 <GlobalStyle />
-
-                {/*
-                ルーティング用のメインコンポーネント
-              */}
+                {/* ルーティング用のメインコンポーネント */}
                 <RootRouter />
               </BrowserRouter>
             </GlobalAccount>
@@ -57,5 +38,5 @@ ReactDOM.render(
       </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
