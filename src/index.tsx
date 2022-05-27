a076357c6923d5e102b4e064578223ed7f130b77
web-novice-tech <itb4769we@gmail.com>
2022-05-27 10:16:43 +0900

    style(/): src/index.tsに関わるESLintのrules, settiingsを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { RootRouter } from './Route';
 import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
-import GlobalStyle from './GlobalStyle';
 import { RecoilRoot } from 'recoil';
+import { RootRouter } from './Route';
+import GlobalStyle from './GlobalStyle';
 import { AuthStateListener } from './providers/AuthStateListener';
 import { GlobalAccount } from './providers/GlobalAccount';
 import { ApolloProvider } from './providers/ApolloClient';
