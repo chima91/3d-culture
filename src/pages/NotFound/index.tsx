7faed3163cede305ec96a504d4540a13e50d185c
web-novice-tech <itb4769we@gmail.com>
2022-07-07 08:09:11 +0900

    refactor(NotFound/): 404ページのスタイルおよびメタデータをを調整

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,13 +2,15 @@ import { Button, Card, Typography } from '@material-ui/core';
 import { VFC } from 'react';
 
 import { Logo } from '../../components/Logo';
-import useStyles from './style';
+import useStyles from '../../components/AuthenticationStyle/style';
+import Head from '../../components/Head';
 
 export const NotFound: VFC = () => {
   const styles = useStyles();
 
   return (
     <Card className={styles.root} variant='outlined'>
+      <Head title='お探しのページは見つかりませんでした。' />
       <div className={`${styles.logo} ${styles.margin}`}>
         <Logo />
       </div>
